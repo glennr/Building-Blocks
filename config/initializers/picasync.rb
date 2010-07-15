2de60954c0a4f2564577f797e25e61f9a1e1329e
Glenn Roberts <glenn.roberts@siyelo.com>
2010-07-15 16:35:22 +0200

    make it more clear when APP_CONFIG bombs out for Picasa init

--- config/initializers/picasync.rb
+++ config/initializers/picasync.rb
@@ -1,5 +1,10 @@
 require 'picasync'
 
-GOOGLE_EMAIL = APP_CONFIG[:google][:picasa][:email]
-GOOGLE_PASS = APP_CONFIG[:google][:picasa][:password]
-GOOGLE_USER = APP_CONFIG[:google][:picasa][:user]
+begin
+  GOOGLE_EMAIL = APP_CONFIG[:google][:picasa][:email]
+  GOOGLE_PASS = APP_CONFIG[:google][:picasa][:password]
+  GOOGLE_USER = APP_CONFIG[:google][:picasa][:user]
+rescue
+  puts "WARNING: Ensure you've set up settings.secret.yml with your Picasa config"
+  raise
+end
